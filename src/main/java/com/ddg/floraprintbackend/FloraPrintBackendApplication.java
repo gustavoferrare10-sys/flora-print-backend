f0bd52f6da88b1a1853c2646940cc4d47c3794fd
Cristiane Costa <gustavoferrare10@gmail.com>
2026-04-24 16:00:15 -0300

    fix: force port from env

--- src/main/java/com/ddg/floraprintbackend/FloraPrintBackendApplication.java
+++ src/main/java/com/ddg/floraprintbackend/FloraPrintBackendApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FloraPrintBackendApplication {
 
     public static void main(String[] args) {
+        String port = System.getenv("PORT");
+        if (port == null) {
+            port = "8080";
+        }
+
+        System.setProperty("server.port", port);
+
         SpringApplication.run(FloraPrintBackendApplication.class, args);
     }
-
 }
