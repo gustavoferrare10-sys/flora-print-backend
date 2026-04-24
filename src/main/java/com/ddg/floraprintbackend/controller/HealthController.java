e3981753bfeafe94a619f9173b2058187a6d32e0
Cristiane Costa <gustavoferrare10@gmail.com>
2026-04-24 16:11:15 -0300

    add health endpoint

--- src/main/java/com/ddg/floraprintbackend/controller/HealthController.java
+++ src/main/java/com/ddg/floraprintbackend/controller/HealthController.java
@@ -1,15 +1,13 @@
 package com.ddg.floraprintbackend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping("/")
     public String health() {
-        return "backend vivo";
+        return "OK";
     }
 }
